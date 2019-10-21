d09562292ae01f52fe6b4cffafd6da02852c6e9c
Victoria Hale <vic.hale74@gmail.com>
2019-10-21 14:31:20 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,15 @@
- class Author 
-  attr_accessor :name 
-  def initialize 
-    @name = name
-  end
-  
   class Post 
     attr_accessor :title
   
     def initialize 
       @title = title 
     end
-  end
+     
+    class Author 
+      attr_accessor :name 
+      
+      def initialize 
+        @name = name
+      end
+    end
 end 
